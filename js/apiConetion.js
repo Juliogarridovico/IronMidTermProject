35b85c33b65c49b5ab805835f194fa32a4f41a3d
gema-munoz <gema.m.moscoso@gmail.com>
2022-08-18 22:02:08 +0200

    api y pruebas

--- js/apiConetion.js
+++ js/apiConetion.js
@@ -1,9 +1,24 @@
 // https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects
 
-fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
-  .then(response => response.json())
-  .then(data => console.log(data));
+let apibody;
 
-
-  console.log(Response)
-
+fetch(
+  "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.forEach((project) => {
+      document.getElementById("Recent-Projects").innerHTML += `
+    <div id="project-1">
+            <img class="rectangle71" src="${project.image}" alt="Orbit section image" />
+            <div class="text-rectangle">
+              <h5 class="Body-Medium">${project.name}</h5>
+              <p class="Headline-Regular">${project.description}</p>
+              <a class="Headline-Regular" href="#">Learn More</a>
+            </div>
+          </div>`;
+      console.log(project);
+    });
+  });
