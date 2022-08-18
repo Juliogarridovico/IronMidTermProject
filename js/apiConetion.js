75af609d4c9c1eb2235b1a2994b918ebb2399320
gema-munoz <gema.m.moscoso@gmail.com>
2022-08-18 20:03:28 +0200

    commit de enlaces

---
+++ js/apiConetion.js
@@ -0,0 +1,9 @@
+// https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects
+
+fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+
+  console.log(Response)
+
